3cfed3f87319e6df44028be2b9c66187f1090d1c
code-xman <1361257125@qq.com>
2023-08-22 00:17:56 +0800

    修改密码， main优化

---
+++ src/components/baseCommon/ModalModifyPassword.vue
@@ -0,0 +1,155 @@
+<template>
+  <el-dialog
+    v-model="modal"
+    title="修改密码"
+    width="540px"
+    :close-on-click-modal="false"
+    append-to-body
+  >
+    <div v-loading="pending" class="modal">
+      <div class="modal-body">
+        <el-form
+          :model="form"
+          :rules="rules"
+          ref="formRef"
+          label-width="120px"
+          style="padding-top: 15px; padding-right: 60px"
+        >
+          <el-form-item label="旧密码" prop="oldPwd">
+            <el-input
+              v-model="form.oldPwd"
+              type="password"
+              placeholder="请输入旧密码"
+              maxlength="32"
+              show-password
+              clearable
+            />
+          </el-form-item>
+          <el-form-item label="新密码" prop="newPwd">
+            <el-input
+              v-model="form.newPwd"
+              type="password"
+              placeholder="请输入新密码"
+              maxlength="32"
+              show-password
+              clearable
+            />
+          </el-form-item>
+          <el-form-item label="确认新密码" prop="confirmPwd">
+            <el-input
+              v-model="form.confirmPwd"
+              type="password"
+              placeholder="请再次输入新密码"
+              maxlength="32"
+              show-password
+              clearable
+            />
+          </el-form-item>
+        </el-form>
+      </div>
+      <div class="modal-footer">
+        <el-button :disabled="pending" @click="modal = false"> 取消</el-button>
+        <el-button type="primary" :disabled="pending" @click="handleSubmit">
+          确定
+        </el-button>
+      </div>
+    </div>
+  </el-dialog>
+</template>
+
+<script setup>
+import { ref, watch } from 'vue';
+import { ElMessage, ElMessageBox } from 'element-plus';
+import { ApiChangePassword } from '@/http/common.js';
+import { clearAuth, getAuthUser } from '@/utils/auth';
+import { encrypte } from '@/utils/common';
+
+defineOptions({ name: 'ModalModifyPassword' });
+
+const emit = defineEmits(['update:modelValue']);
+const props = defineProps({
+  modelValue: Boolean,
+});
+const _passwordRule = {
+  pattern: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{6,20}$/,
+  message: '密码必需包含大小写字母、数字,长度6-20',
+};
+
+const modal = ref(false);
+const pending = ref(false);
+const formRef = ref(null);
+const form = ref({});
+const rules = ref({
+  oldPwd: [{ required: true, message: '请输入旧密码' }, _passwordRule],
+  newPwd: [{ required: true, message: '请输入新密码' }, _passwordRule],
+  confirmPwd: [{ required: true, message: '请再次输入新密码' }, _passwordRule],
+});
+
+const handleSubmit = async () => {
+  try {
+    // 表单校验
+    await new Promise((resolve, reject) => {
+      formRef.value?.validate((valid) => {
+        valid ? resolve(true) : reject('cancel');
+      });
+    });
+
+    await ElMessageBox.confirm('确定保存当前修改？', '操作提示', {
+      type: 'warning',
+    });
+
+    pending.value = true;
+
+    // 加密处理
+    const _params = {
+      ...form.value,
+      oldPwd: encrypte(form.value.oldPwd),
+      newPwd: encrypte(form.value.newPwd),
+      confirmPwd: encrypte(form.value.confirmPwd),
+    };
+
+    await ApiChangePassword(_params);
+
+    ElMessage({
+      type: 'success',
+      message: '密码更新成功，请重新登录',
+      duration: 1000,
+      onClose: async () => {
+        ApiLogout();
+        clearAuth();
+        await new Promise((resolve) => {
+          setTimeout(() => {
+            resolve(true);
+          }, 600);
+        });
+        window.location.href = '/login';
+      },
+    });
+  } catch (error) {
+    if (error === 'cancel') return;
+    ElMessage.error(`${error}`);
+  } finally {
+    pending.value = false;
+  }
+};
+
+watch(
+  () => props.modelValue,
+  (value) => {
+    modal.value = value;
+  }
+);
+
+watch(
+  () => modal,
+  () => {
+    if (value) {
+      const user = getAuthUser();
+      form.value.userId = user.userId;
+    } else {
+      form.value = {};
+    }
+    emit('update:modelValue', value);
+  }
+);
+</script>
--- src/layout/Main.vue
+++ src/layout/Main.vue
@@ -2,19 +2,23 @@
   <div class="main">
     <div class="header">
       <img class="logo" src="@/assets/vue.svg" />
-      <div class="home item">{{ APP_INFO.title }}</div>
+      <div class="home item">{{ user.sysName || APP_INFO.title }}</div>
       <div v-if="route.meta.title" class="title">
         {{ route.meta.title || '' }}
       </div>
       <div class="flex-1"></div>
       <!-- <div class="item">个人中心</div> -->
+      <div class="user">
+        <i class="icon iconfont icon-yonghuguanli" />
+        {{ user.userName }}
+      </div>
       <el-dropdown class="item" @command="handleCommand">
         操作
         <template #dropdown>
           <el-dropdown-menu>
-            <!-- <el-dropdown-item command="ModifyPassword">
+            <el-dropdown-item command="ModifyPassword">
               修改密码
-            </el-dropdown-item> -->
+            </el-dropdown-item>
             <el-dropdown-item command="Logout">
               <span class="color-red"> 退出登录 </span>
             </el-dropdown-item>
@@ -29,23 +33,34 @@
       </div>
     </div>
   </div>
+  
+  <!-- 弹窗 :: 修改密码 -->
+  <ModalModifyPassword v-model="modalModifyPassword" />
 </template>
 
 <script setup>
+import { ref } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
+import { ElMessage, ElMessageBox } from 'element-plus';
 import { APP_INFO, CACHE_PREFIX, CACHE_AUTH_PREFIX } from '@/config/base';
 import Menu from '@/components/menu/index.vue';
+import ModalModifyPassword from '@/components/baseCommon/ModalModifyPassword';
 import { removeCache } from '@/utils/common.js';
 import { removeAuthToken, clearAuth } from '@/utils/auth.js';
-import { ElMessage, ElMessageBox } from 'element-plus';
+import { getAuthUser } from '@/utils/auth';
 
 const route = useRoute();
 const router = useRouter();
+const user = getAuthUser();
+const modalModifyPassword = ref(false);
 
 // 处理操作
 const handleCommand = async (command) => {
   switch (command) {
     case 'ModifyPassword':
+      {
+        modalModifyPassword.value = true;
+      }
       break;
 
     case 'Logout':
@@ -101,12 +116,25 @@ const handleCommand = async (command) => {
     max-width: 100%;
   }
 
+  .user {
+    font-size: 14px;
+    display: flex;
+    align-items: center;
+    .icon {
+      margin-right: 4px;
+      font-size: 20px;
+    }
+  }
+
   .item {
     padding: 0 16px;
     cursor: pointer;
     font-size: 14px;
     &.el-dropdown {
+      padding-right: 0;
+      display: inline-block;
       color: #fff;
+      line-height: 56px;
     }
   }
 
